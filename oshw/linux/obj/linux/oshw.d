59a2ea4df7caa4e5ab80082d401d306ce3c0c08c
jumorean <49334068+jumorean@users.noreply.github.com>
2019-11-01 11:50:38 +0800

    Add files via upload

---
+++ oshw/linux/obj/linux/oshw.d
@@ -0,0 +1,18 @@
+obj/linux/oshw.d: oshw.c oshw.h \
+ /home/jumorean/桌面/SOEM源码/soem_github/soem/ethercattype.h \
+ /home/jumorean/桌面/SOEM源码/soem_github/osal/osal.h \
+ /home/jumorean/桌面/SOEM源码/soem_github/osal/linux/osal_defs.h \
+ nicdrv.h \
+ /home/jumorean/桌面/SOEM源码/soem_github/soem/ethercatmain.h
+
+oshw.h:
+
+/home/jumorean/桌面/SOEM源码/soem_github/soem/ethercattype.h:
+
+/home/jumorean/桌面/SOEM源码/soem_github/osal/osal.h:
+
+/home/jumorean/桌面/SOEM源码/soem_github/osal/linux/osal_defs.h:
+
+nicdrv.h:
+
+/home/jumorean/桌面/SOEM源码/soem_github/soem/ethercatmain.h:
